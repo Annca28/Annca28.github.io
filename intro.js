ef6d76140e8d057e33e2301542293ed9a70e8932
Anca Botan <Annca28>
2019-03-18 19:40:49 +0200

    showallpages

--- intro.js
+++ intro.js
@@ -10,6 +10,7 @@ function getColor() {
         return "green"
     }
     return "blue";
+}
 
 
 var wellcome = getWelcolmeMsg();
@@ -22,11 +23,6 @@ var color = getColor();
 summaryElement.style.color = color;
 
 
-
-
-
-
-
 function hidePage(page){
     var el = document.getElementById(page);
     el.style.display= 'none';
@@ -34,3 +30,28 @@ function hidePage(page){
 function showPage(page){
     document.getElementById(page).style.display = 'block';
 }
+
+
+function initMenu() {
+    var links = document.querySelectorAll("#top-menu-bar a");
+    console.info(links);
+    for(var i = 0; i < links.length; i++) {
+        links[i].onclick = clickOnMenuItem;
+    }
+}
+
+function clickOnMenuItem () {
+    console.warn('clicked on menu', this);
+    hideAllPages();
+    var pageId = this.getAttribute('data-page');
+    console.warn({pageId});
+    showPage(pageId);
+}
+
+function hideAllPages() {
+    var pages = document.querySelectorAll('.page');
+    for(var i = 0; i < pages.length; i++) {
+        pages[i].style.display = "none";
+    }
+}
+initMenu();
